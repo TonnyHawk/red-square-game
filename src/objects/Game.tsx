92f6d8de140d06b947fc2bf4c726e6d22d954600
Anthonii Veremko <anton.veremko@gmail.com>
2023-01-01 19:20:44 +0100

    many inventory item were added to the players inventory at the init phase

--- src/objects/Game.tsx
+++ src/objects/Game.tsx
@@ -59,6 +59,28 @@ export default class Game {
 		this.items.push(new BulletItem(this, 200, 300));
 		this.items.push(new ChickenLeggItem(this, 300, 300));
 		this.items.push(new ChickenLeggItem(this, 250, 300));
+		this.player.collect(new ChickenLeggItem(this, 300, 300));
+		this.player.collect(new BulletItem(this, 300, 300));
+		this.player.collect(new ChickenLeggItem(this, 300, 300));
+		this.player.collect(new BulletItem(this, 300, 300));
+		this.player.collect(new ChickenLeggItem(this, 300, 300));
+		this.player.collect(new BulletItem(this, 300, 300));
+		this.player.collect(new ChickenLeggItem(this, 300, 300));
+		this.player.collect(new BulletItem(this, 300, 300));
+		this.player.collect(new ChickenLeggItem(this, 300, 300));
+		this.player.collect(new BulletItem(this, 300, 300));
+		this.player.collect(new ChickenLeggItem(this, 300, 300));
+		this.player.collect(new BulletItem(this, 300, 300));
+		this.player.collect(new ChickenLeggItem(this, 300, 300));
+		this.player.collect(new BulletItem(this, 300, 300));
+		this.player.collect(new ChickenLeggItem(this, 300, 300));
+		this.player.collect(new BulletItem(this, 300, 300));
+		this.player.collect(new ChickenLeggItem(this, 300, 300));
+		this.player.collect(new BulletItem(this, 300, 300));
+		this.player.collect(new ChickenLeggItem(this, 300, 300));
+		this.player.collect(new BulletItem(this, 300, 300));
+		this.player.collect(new ChickenLeggItem(this, 300, 300));
+		this.player.collect(new BulletItem(this, 300, 300));
 		this.draw();
 	}
 
